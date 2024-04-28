e310b620ee16576f0390e80eee0a752d80da29e1
John Christian Natividad <120337618+jaycee0610@users.noreply.github.com>
2024-04-28 23:40:33 +0800

    Get Device(s)

--- PHP/Get_Device.php
+++ PHP/Get_Device.php
@@ -5,7 +5,7 @@ include_once "function.php";
 try {
     // Get all enabled devices for sending messages.
     $devices = getDevices()
-    print_r($devices);
+   // print_r($devices);
 } catch (Exception $e) {
     echo $e->getMessage();
 }
